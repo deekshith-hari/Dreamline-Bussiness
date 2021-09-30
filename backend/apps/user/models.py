a297cbfdb364444baa022db80e315b133f1af667
Stevy <superstevy@yahoo.com>
2021-09-30 18:56:29 -0400

    created user app and added UserAccount model

---
+++ backend/apps/user/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserAccount(models.Model):
+    class Meta(object):
+        db_table = 'userdb'
+
+    username = models.CharField(
+        'Username', max_length=50, blank=False, unique=True, db_index=True
+    )
+    password = models.CharField(
+        'Password', max_length=500, blank=False, db_index=True
+    )
+    email = models.EmailField(
+        'Email', max_length=254, blank=False, db_index=True
+    )
+    token = models.CharField(
+        'Token', max_length=500
+    )
+    token_expires_at = models.DateTimeField(
+        'Token Expires At', blank=True
+    )
+    created_at = models.DateTimeField(
+        'Created At', blank=True, auto_now_add=True
+    )
+    modified_at = models.DateTimeField(
+        'Modified At', blank=True, auto_now=True
+    )
+    user = models.ForeignKey(
+        User, related_name='user', on_delete=models.CASCADE, null=True, db_index=True
+    )
